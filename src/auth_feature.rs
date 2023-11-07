8a562cb7c5bcd372d89d306816ad2add758acb03
Eduardo P de Sousa <edupsousa@gmail.com>
2023-11-07 17:53:08 -0300

    create login post route

--- src/auth_feature.rs
+++ src/auth_feature.rs
@@ -1,6 +1,16 @@
 use askama::Template;
 use askama_axum::IntoResponse;
+use axum::{Form, http::StatusCode};
+use validator::Validate;
+use serde::Deserialize;
 
+#[derive(Deserialize, Validate)]
+pub struct LoginData {
+    #[validate(length(min = 3))]
+    username: String,
+    #[validate(length(min = 8))]
+    password: String,
+}
 
 #[derive(Template)]
 #[template(path = "login.html")]
@@ -8,4 +18,17 @@ struct LoginTemplate;
 
 pub async fn get_login_page() -> impl IntoResponse {
     LoginTemplate {}
+}
+
+pub async fn post_login(Form(form): Form<LoginData>) -> impl IntoResponse {
+    let validation = form.validate();
+    return match validation {
+      Ok(_) => {
+        println!("Login success");
+        LoginTemplate {}.into_response()
+      },
+      Err(_) => {
+        (StatusCode::BAD_REQUEST, LoginTemplate {}).into_response()
+      }
+    };
 }
