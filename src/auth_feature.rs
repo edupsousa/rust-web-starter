2ffee6a05fbb20667e13cfc5e0a4fdbc2a7db419
Eduardo P de Sousa <edupsousa@gmail.com>
2023-11-07 18:04:44 -0300

    display validation error

--- src/auth_feature.rs
+++ src/auth_feature.rs
@@ -1,8 +1,8 @@
 use askama::Template;
 use askama_axum::IntoResponse;
-use axum::{Form, http::StatusCode};
-use validator::Validate;
+use axum::Form;
 use serde::Deserialize;
+use validator::Validate;
 
 #[derive(Deserialize, Validate)]
 pub struct LoginData {
@@ -14,21 +14,20 @@ pub struct LoginData {
 
 #[derive(Template)]
 #[template(path = "login.html")]
-struct LoginTemplate;
+pub struct LoginTemplate {
+    pub error: Option<String>,
+}
 
 pub async fn get_login_page() -> impl IntoResponse {
-    LoginTemplate {}
+    LoginTemplate { error: None }
 }
 
 pub async fn post_login(Form(form): Form<LoginData>) -> impl IntoResponse {
     let validation = form.validate();
     return match validation {
-      Ok(_) => {
-        println!("Login success");
-        LoginTemplate {}.into_response()
-      },
-      Err(_) => {
-        (StatusCode::BAD_REQUEST, LoginTemplate {}).into_response()
-      }
+        Ok(_) => LoginTemplate { error: None },
+        Err(_) => LoginTemplate {
+            error: Some("Bad request!".to_string()),
+        },
     };
 }
